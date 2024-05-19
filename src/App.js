a9791021f8a5a43eac918b8a7856966d68ca7737
Burakkturk <burak1845turk@icloud.com>
2024-05-19 11:17:56 +0300

    Button added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import { Button } from "./components/Button.styled";
+import { Button, DarkButton } from "./components/Button.styled";
 
 const App = () => {
   return (
     <>
       <h1>Styled Components</h1>
       <Button>Tıkla</Button>
+      <DarkButton>Send</DarkButton>
     </>
   );
 };
--- src/components/Button.styled.jsx
+++ src/components/Button.styled.jsx
@@ -2,7 +2,25 @@ import styled from "styled-components"
 
 
 export const Button = styled.button`
-background-color: hotpink
+background-color: hotpink;
+color: white ;
+border: none;
+font-size: 1.3rem;
+padding: 0.7rem 3rem;
+border-radius: 20px;
+font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+&:hover{
+    cursor: pointer;
+    transform: scale(0.96);
+}
 
 
 `
+export const DarkButton = styled(Button)`
+background-color: black;
+color: white;
+
+
+`
+
+
