323a8b8116f573e83d26d8f1d4ca6c1af6be58af
Burakkturk <burak1845turk@icloud.com>
2024-05-24 22:47:33 +0300

    feat:link

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import { Button, DarkButton } from "./components/Button.styled";
+import {  DarkButton, PinkButton } from "./components/Button.styled";
+import { Button } from "./components/Button.styled";
+// import styled from 'styled-components/native'
+import Flex from "./components/Flex"
+import StyledLink from "./components/Links";
+import Title from "./components/Title"
+
 
 const App = () => {
   return (
-    <>
+   <>
+    <Flex bg="#bebe">
+      <Title renk="white" bgRenk="red">
+        Styled Components
+      </Title>
       <h1>Styled Components</h1>
-      <Button>Tıkla</Button>
+      <Button>Tikla</Button>
       <DarkButton>Send</DarkButton>
-    </>
+      
+      </Flex>
+      <Title renk="green">Props</Title>
+      <Title bgRenk="purple">Props</Title>
+
+      <Flex bg="tomato">
+        <h1>PROPS</h1>
+      </Flex>
+      <Flex>
+      <PinkButton>Pembe</PinkButton>
+      <PinkButton primary>İçi boş</PinkButton>
+      </Flex>
+      <Flex>
+        <StyledLink href="https://gsstore.org">BT</StyledLink>
+      </Flex>
+      </>
   );
 };
 
--- src/components/Button.styled.jsx
+++ src/components/Button.styled.jsx
@@ -3,24 +3,36 @@ import styled from "styled-components"
 
 export const Button = styled.button`
 background-color: hotpink;
-color: white ;
-border: none;
+color: white;
+border: 2px solid hotpink;
 font-size: 1.3rem;
 padding: 0.7rem 3rem;
 border-radius: 20px;
 font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
-&:hover{
-    cursor: pointer;
-    transform: scale(0.96);
+&:hover {
+cursor: pointer;
+transform: scale(0.96);
 }
+`
 
 
+
+
+export const PinkButton = styled.button`
+background-color: ${({primary}) => ( primary ? "white" : "hotpink" )};
+color: ${({primary}) => ( primary ? "hotpink" : "white" )};
+border: 2px solid hotpink;
+font-size: 1.3rem;
+padding: 0.7rem 3rem;
+border-radius: 20px;
+font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+&:hover {
+cursor: pointer;
+transform: scale(0.96);
+}
 `
 export const DarkButton = styled(Button)`
 background-color: black;
-color: white;
-
-
-`
-
-
+color: white; 
+border: none;
+` 
